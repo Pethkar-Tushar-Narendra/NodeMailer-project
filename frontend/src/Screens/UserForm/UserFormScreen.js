a6e33cf3ef3301bd99cf8c244dbca44ed65981ae
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2022-12-20 18:10:57 +0530

    commit

--- frontend/src/Screens/UserForm/UserFormScreen.js
+++ frontend/src/Screens/UserForm/UserFormScreen.js
@@ -1,7 +1,44 @@
 import React from 'react';
-
+import './UserFormScreen.css';
 const UserFormScreen = () => {
-  return <div>UserFormScreen</div>;
+  return (
+    <div className="form-container">
+      <h1>User Form</h1>
+      <div className="form">
+        <h2>Fill the form to get Email</h2>
+        <form>
+          <div className="box">
+            <label>Name</label>
+            <input type="text" placeholder="Enter Name"></input>
+            <div className="errBox" style={{ display: 'none' }}>
+              error
+            </div>
+          </div>
+          <div className="box">
+            <label>E-Mail</label>
+            <input type="text" placeholder="Enter Email"></input>
+            <div className="errBox" style={{ display: 'none' }}>
+              error
+            </div>
+          </div>
+          <div className="box">
+            <label>Mobile Number</label>
+            <input type="number" placeholder="Enter Mobile Number"></input>
+            <div className="errBox" style={{ display: 'none' }}>
+              error
+            </div>
+          </div>
+          <div className="box">
+            <label>Date Of Birth</label>
+            <input type="text" placeholder="Date Of Birth"></input>
+            <div className="errBox" style={{ display: 'none' }}>
+              error
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UserFormScreen;
